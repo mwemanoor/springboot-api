01b5ff6d77e4e441f7d3bdf7046363bd30d7e1c8
mwemanoor <mwemanoor@gmail.com>
2025-04-26 17:40:32 +0300

    Updated the bash scripts

--- rest-api/bash_scripts/backup_api.sh
+++ rest-api/bash_scripts/backup_api.sh
@@ -6,7 +6,7 @@ BACKUP_DIR="/home/ubuntu/backups"
 mkdir -p $BACKUP_DIR
 
 # Backup API files
-tar -czf "$BACKUP_DIR/api_backup_$(date +%F).tar.gz" /home/ubuntu/rest-api-*.jar
+tar -czf "$BACKUP_DIR/api_backup_$(date +%F).tar.gz" --absolute-names /home/ubuntu/rest-api-*.jar
 if [ $? -eq 0 ]; then
     echo "[$TIMESTAMP] API backup succeeded." >> $LOG_FILE
 else
@@ -14,7 +14,7 @@ else
 fi
 
 # Backup PostgreSQL
-sudo -u postgres pg_dump rest_api > "$BACKUP_DIR/db_backup_$(date +%F).sql"
+sudo -u postgres pg_dump rest-api > "$BACKUP_DIR/db_backup_$(date +%F).sql"
 if [ $? -eq 0 ]; then
     echo "[$TIMESTAMP] Database backup succeeded." >> $LOG_FILE
 else
